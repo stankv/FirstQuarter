904250144a270f3dcb30bb7c9008c819c332cc6c
Stan Bad <stankv@mail.ru>
2022-12-26 13:13:47 +0300

    Fixed magic number 3

--- Program.cs
+++ Program.cs
@@ -12,24 +12,24 @@
 }
 
 
-int CountNeedItems(string[] arr)
+int CountNeedItems(string[] arr, int len_string)
 {
     int count = 0;
-    foreach (string item in arr) if (item.Length <= 3) count++;
+    foreach (string item in arr) if (item.Length <= len_string) count++;
     return count;
 }
 
 
-string[] FillOutputArray(string[] arr)
+string[] FillOutputArray(string[] arr, int len_string)
 {
-    int length = CountNeedItems(arr);
+    int length = CountNeedItems(arr, len_string);
     string[] array = new string[length];
     if (length == 0) return array;
 
     int count = 0;
     foreach (string item in arr)
     {
-        if (item.Length <= 3)
+        if (item.Length <= len_string)
         {
             array[count] = item;
             count++;
@@ -53,9 +53,10 @@ void PrintArray(string[] arr)
 }
 
 
+int len_string = 3;    // максимальное количество символов в строке
 string[] array_in = InputArray();
 Console.Clear();
 Console.Write("Input Array:");
 PrintArray(array_in);
 Console.Write("Output Array:");
-PrintArray(FillOutputArray(array_in));
+PrintArray(FillOutputArray(array_in, len_string));
